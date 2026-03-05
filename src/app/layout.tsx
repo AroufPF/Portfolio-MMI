e10b8463b70da2482f6cf11e7a0a889d79d5545d
AroufPF <anas.benhaddane@univ-rouen.fr>
2026-03-05 07:52:16 +0100

    fix: provider audio layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// 👇 ON IMPORTE LE CERVEAU DE L'AUDIO
+import { AudioProvider } from "../context/AudioContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,17 +21,16 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-slate-950 text-slate-300 antialiased`}
       >
-        {/* L'effet de scan (Laser qui descend) */}
-        <div className="cyber-scan"></div>
+        {/* ⚙️ ON ENVELOPPE TOUT LE CONTENU POUR FIXER L'ERREUR VERCEL */}
+        <AudioProvider>
+          {/* Tes effets visuels Cyberlife */}
+          <div className="cyber-scan"></div>
+          <div className="cyber-grid"></div>
+          <div className="cyber-vignette"></div>
 
-        {/* La grille de fond */}
-        <div className="cyber-grid"></div>
-
-        {/* L'effet d'ombre sur les bords */}
-        <div className="cyber-vignette"></div>
-
-        {/* Le contenu principal */}
-        <div className="relative z-10">{children}</div>
+          {/* Le contenu principal */}
+          <div className="relative z-10">{children}</div>
+        </AudioProvider>
       </body>
     </html>
   );
